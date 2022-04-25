55340de536e2a33443af6680f905fa0faccec4ba
Mohamed hassan kadri <kadri.mohamedhasan@gmail.com>
2022-04-25 02:32:34 +0100

    adding descripition

--- lib/ui/shared/base_button.dart
+++ lib/ui/shared/base_button.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 abstract class BaseButton {
+  //a boolean used to disable the button
   bool? disabled;
+  //a double used to determine the border radius
   double? radius;
+  // a string used to determine the main string in a button
   String? buttonText;
+  // color code for the font
   Color? fontColor;
+  // color code for the button background
   Color? mainColor;
+  // color code for the border
   Color? borderColor;
+  // an Icon
   IconData? icon;
+  //a boolean used to determine if the button is rounded or rectangle
   bool? rounded;
 }
